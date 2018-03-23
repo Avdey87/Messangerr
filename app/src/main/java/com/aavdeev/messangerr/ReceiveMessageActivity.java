3f07df9da308eff83ec7fa670d2b08829894dab5
avdey <avdey87@gmail.com>
2018-03-23 16:44:59 +0300

    page 133

--- app/src/main/java/com/aavdeev/messangerr/ReceiveMessageActivity.java
+++ app/src/main/java/com/aavdeev/messangerr/ReceiveMessageActivity.java
@@ -1,13 +1,21 @@
 package com.aavdeev.messangerr;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class ReceiveMessageActivity extends AppCompatActivity {
+public class ReceiveMessageActivity extends Activity {
 
+    public static final String EXTRA_MESSAGE = "message";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+        Intent intent = getIntent();
+        String string = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView tv = findViewById(R.id.message);
+        tv.setText(string);
+
     }
 }
